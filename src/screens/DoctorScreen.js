92bacc17672e6506b1c1d78b2257882a17c308b0
Melvin Mathews <mel.matt007@gmail.com>
2018-09-16 06:40:52 -0700

    trending page and patient info logging implemented

--- src/screens/DoctorScreen.js
+++ src/screens/DoctorScreen.js
@@ -1,27 +1,120 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoDoctorView } from '@expo/samples';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default class DoctorScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Doctor',
-  };
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Dimensions } from 'react-native'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph
+} from 'react-native-chart-kit'
+
+import data from './data.json';
+
+
+//const heartrateData = this.getHeartrateData()
+//var A = JSON.parse(data);
+var C = data.val;
+export default class App extends Component{
 
   render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoDoctorView and replace it with your
-           * content, we just wanted to provide you with some helpful Doctor */}
-        <ExpoDoctorView />
-      </ScrollView>
+    
+    return (      
+    <View style={styles.container}>
+      <Text style={styles.getStartedText}>
+        Heartbeats per Second
+      </Text>
+      <LineChart
+          data={{
+          labels: ['10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm'],
+          datasets: [{
+            
+            
+            data: [50, 100, 110, 70, 40, 90]
+
+          }]
+        }}
+        width={Dimensions.get('window').width} // from react-native
+        height={220}
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+
+      <Text style={styles.getStartedText}>
+        Anxiety Level
+      </Text>
+      <LineChart
+        data={{
+          labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+          datasets: [{
+            
+            data:[5, 3, 1, 1, 5, 2, 3]
+
+          }]
+        }}
+        width={Dimensions.get('window').width} // from react-native
+        height={220}
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          }
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+    </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
   },
 });
--- src/screens/PatientScreen.js
+++ src/screens/PatientScreen.js
@@ -1,14 +1,117 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from 'react'
+import { Button, Alert, StyleSheet, Text, View } from 'react-native'
+import Form from 'react-native-advanced-forms'
 
-export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
-  };
+export default class App extends React.Component {
+  constructor (props, ctx) {
+    super(props, ctx)
+
+    this.state = {
+      firstName: null,
+      lastName: null,
+      age: null,
+      country: null,
+    }
+  }
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    const {
+      firstName, lastName, age, country
+    } = this.state
+
+    return (
+      <View style={styles.container}>
+        <Form ref={this._onFormRef} onChange={this.onChange} onSubmit={this.onSubmit} validate={this.validate}>
+          <Form.Layout style={styles.row}>
+            <Form.Layout style={styles.columns}>
+              <Form.Field name="firstName" label="First name" style={styles.field}>
+                <Form.TextField value={firstName} />
+              </Form.Field>
+              <Form.Field name="lastName" label="Last name" style={styles.field}>
+                <Form.TextField value={lastName} />
+              </Form.Field>
+            </Form.Layout>
+          </Form.Layout>
+          <Form.Layout style={styles.row}>
+            <Form.Field name="age" label="Age" style={styles.ageField}>
+              <Form.TextField value={age} keyboardType='numeric'/>
+            </Form.Field>
+          </Form.Layout>
+          <Form.Layout style={styles.row}>
+            <Form.Field name="country" label="Country" style={styles.field}>
+              <Form.TextField value={country} />
+            </Form.Field>
+          </Form.Layout>
+        </Form>
+        <View style={styles.button}>
+          <Button
+            disabled={this.form && !this.form.canSubmit()}
+            onPress={() => this.form.validateAndSubmit()}
+            title="Submit"
+            color="#841584"
+          />
+        </View>
+      </View>
+    )
+  }
+
+  _onFormRef = e => {
+    this.form = e
+  }
+
+  onChange = (values) => {
+    this.setState(values)
+  }
+
+  onSubmit = (values) => {
+    Alert.alert('Submitted: ' + JSON.stringify(values))
+  }
+
+  validate = (values) => {
+    const ret = Object.keys(this.state).reduce((m, v) => {
+      if (!values[v] || !values[v].length) {
+        m[v] = Form.VALIDATION_RESULT.MISSING
+      }
+      return m
+    }, {})
+
+    if (!ret.age && isNaN(values.age)) {
+      ret.age = Form.VALIDATION_RESULT.INCORRECT
+    }
+
+    return ret
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 100,
+    paddingHorizontal: 30
+  },
+  row: {
+    marginBottom: 20,
+  },
+  columns: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  field: {
+    marginRight: 10,
+  },
+  ageField: {
+    width: 60,
+  },
+  button: {
+    width: 80,
+    marginTop: 15,
+  },
+  error: {
+    marginTop: 10,
+  },
+  errorMsg: {
+    color: 'red'
+  }
+})
