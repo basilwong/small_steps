067f0a402e3f0cb7f2ad30ee0841d4e35c5499b7
basilwong <wong.bas@gmail.com>
2018-09-15 20:51:52 -0700

    Added dependancy to readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lumohacks2018
+
+Required for the table component on doctor's page:
+
+```npm install react-native-table-component```
--- src/screens/DoctorScreen.js
+++ src/screens/DoctorScreen.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoDoctorView } from '@expo/samples';
+import { AppRegistry, ScrollView, Image, Text, StyleSheet } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
+// import { ExpoDoctorView } from '@expo/samples';
 
 export default class DoctorScreen extends React.Component {
   static navigationOptions = {
-    title: 'Doctor',
+    title: 'Patient List',
   };
+
+  // export default class ExampleOne extends Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       tableHead: ['Head', 'Head2', 'Head3', 'Head4'],
+  //       tableData: [
+  //         ['1', '2', '3', '4'],
+  //         ['a', 'b', 'c', 'd'],
+  //         ['1', '2', '3', '456\n789'],
+  //         ['a', 'b', 'c', 'd']
+  //       ]
+  //     }
+  //   }
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoDoctorView and replace it with your
-           * content, we just wanted to provide you with some helpful Doctor */}
-        <ExpoDoctorView />
+        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
+          <Rows data={state.tableData} textStyle={styles.text}/>
+        </Table>
       </ScrollView>
     );
   }
