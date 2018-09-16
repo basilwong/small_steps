59c6053b9452e37cb9142956ac028ca342c81742
Melvin Mathews <mel.matt007@gmail.com>
2018-09-16 12:44:32 -0700

    configuration changes to Trends/Doctors Screen

--- src/screens/TrendsScreen.js
+++ src/screens/TrendsScreen.js
@@ -1,48 +1,124 @@
-import React, { Component } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  KeyboardAvoidingView
-} from "react-native";
-import LoginForm from "./LoginForm";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default class Login extends Component {
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Dimensions } from 'react-native'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph
+} from 'react-native-chart-kit'
+
+import heartData from './data.json';
+
+
+//const heartrateData = this.getHeartrateData()
+//var A = JSON.parse(data);
+
+
+export default class App extends Component{
+
   render() {
-    return (
-      <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <View style={styles.logoContainer}>
-          <Image style={styles.logo} source={require("../img/Logo.png")} />
-          <Text style={styles.description}> Trends</Text>
-        </View>
-        <View style={styles.formContainer}>
-          <LoginForm />
-        </View>
-      </KeyboardAvoidingView>
+    
+    data = heartData.map(function (i) { return i })
+    
+    return (      
+    <View style={styles.container}>
+      <Text style={styles.getStartedText}>
+        Heartbeats per Second
+      </Text>
+      <LineChart
+          data={{
+          labels: ['10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm'],
+          datasets: [{
+            
+             
+              data: heartData.map(function (i) { return i })
+                    
+
+          }]
+        }}
+        width={Dimensions.get('window').width} // from react-native
+        height={220}
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+
+      <Text style={styles.getStartedText}>
+        Anxiety Level
+      </Text>
+      <LineChart
+        data={{
+          labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+          datasets: [{
+            
+            data:[5, 3, 1, 1, 5, 2, 3]
+
+          }]
+        }}
+        width={Dimensions.get('window').width} // from react-native
+        height={220}
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          }
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+    </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#3498db"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  logoContainer: {
-    alignItems: "center",
-    flexGrow: 1,
-    justifyContent: "center"
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  logo: {
-    width: 120,
-    height: 120
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
-  description: {
-    color: "white",
-    marginTop: 10,
-    width: 160,
-    textAlign: "center",
-    opacity: 0.9
-  }
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
 });
