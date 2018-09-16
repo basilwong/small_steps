008e7ddb59c919d69160a5d60ad3c851c29ed79f
Melvin Mathews <mel.matt007@gmail.com>
2018-09-16 00:08:35 -0700

    first iteration of trend screen

---
+++ src/screens/TrendsScreen.js
@@ -0,0 +1,76 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow
+//  */
+
+// import React, {Component} from 'react';
+// import {Platform, StyleSheet, Text, View} from 'react-native';
+// import { VictoryChart } from "victory-native";
+// import { VictoryTheme } from "victory-native";
+// import { VictoryArea } from "victory-native";
+// import { VictoryAxis } from "victory-native";
+
+// //import data from './data.json';
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+// //const heartrateData = this.getHeartrateData()
+
+// export default class App extends Component{
+
+// //   componentDidMount() {
+
+// //     const Heartdata={data};
+// //     alert(Heartdata);
+    
+// //   }
+
+//   render() {
+    
+//     return (      
+//       <View style={styles.container}>
+//          <VictoryChart
+//           theme={VictoryTheme.material}
+//         >
+//           <VictoryArea data={[
+//             {x: 1, y: 4},
+//             {x: 2, y: 3},
+//             {x: 3, y: 5},
+//             {x: 4, y: 6},
+//             {x: 5, y: 1}
+             
+//           ]}/>
+
+//           <VictoryAxis/>
+//         </VictoryChart> 
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
