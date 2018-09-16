850cfdf9ca07e3097c83e9b6cc2d410992a4c22f
basilwong <wong.bas@gmail.com>
2018-09-16 10:01:56 -0700

    Cleaning up modules, for faster pulling/pushing

---
+++ node_modules/react-native-chart-kit/index.js
@@ -0,0 +1,16 @@
+import 'babel-polyfill'
+import AbstractChart from './src/abstract-chart'
+import LineChart from './src/line-chart'
+import BarChart from './src/bar-chart'
+import PieChart from './src/pie-chart'
+import ProgressChart from './src/progress-chart'
+import ContributionGraph from './src/contribution-graph'
+
+export {
+  AbstractChart,
+  BarChart,
+  LineChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph
+}
